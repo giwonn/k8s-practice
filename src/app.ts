689a8aa0724d97e281abad729a5bc78bd333608e
Allen <allendev@alphacode.co.kr>
2022-08-01 18:38:51 +0900

    k8s 선언적 파일 작성

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+
+const app = express(); 
+
+const sendData = "hello world! 수정본";
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(sendData);
+});
+
+app.get('/error', (req, res) => {
+  process.exit(1)
+})
+
+app.listen(8082);
