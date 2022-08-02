3b0abfbc22cbc0ef35bb4fc82c297f9fec72fb44
Allen <allendev@alphacode.co.kr>
2022-08-02 13:29:32 +0900

    활성 프로브 작성

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const app = express(); 
 
-const sendData = "hello world! 수정본";
+const sendData = "hello world! 수정본2";
 
 app.get("/", (req: Request, res: Response) => {
   res.send(sendData);
